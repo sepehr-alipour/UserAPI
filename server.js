042be1ae1ebc4e5f7ae869138c43f4c37ea3ecf7
Sepehr Alipour <alipour.dev@gmail.com>
2022-12-02 23:36:37 -0500

    removed roles

--- server.js
+++ server.js
@@ -25,8 +25,6 @@ let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
         next(null, {
             _id: jwt_payload._id,
             userName: jwt_payload.userName,
-            fullName: jwt_payload.fullName,
-            role: jwt_payload.role,
         });
     } else {
         next(null, false);
